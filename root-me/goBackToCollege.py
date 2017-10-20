43605694578e51b433dc46dba8cbd98c02a11d0a
awayuki9 <awayukibaka@gmail.com>
2017-10-20 10:11:07 -0400

    Update goBackToCollege.py

--- root-me/goBackToCollege.py
+++ root-me/goBackToCollege.py
@@ -8,8 +8,10 @@ name = "SnowNeko9"
 bot  = "candy"
 
 print conn.recvuntil('instead.')
+print 'send NICK'
 conn.send('NICK {0}\r\n'.format(name))
-conn.send('USER {0} irc.root-me.org root-me :ChallengeBot\r\n'.format(name))
+print 'send USER'
+conn.send('USER Awayuki * * :ChallengeBot\r\n') #'USER {0} irc.root-me.org root-me :ChallengeBot\r\n'
 conn.send('JOIN #root-me_challenge')
 
 print conn.recvuntil('MODE {0}'.format(name))
@@ -29,6 +31,20 @@ ans = round(math.sqrt(int(cal[2])) * int(cal[3]),2)
 conn.send('PRIVMSG {0} :!ep1 -rep {1}\r\n'.format( bot ,str(ans).encode("ASCII") ) )
 
 print conn.recvline()
+
+
+while(True):
+	rev=conn.recvline()
+	print rev
+	cal = rev.replace(':','/').replace(' ','').replace('\r\n','').split('/')
+	print cal
+	if cal[0]=='PING':
+		print 'Send PONG'
+		conn.send('PONG\r\n')
+		
+
+
+
 
 
 
